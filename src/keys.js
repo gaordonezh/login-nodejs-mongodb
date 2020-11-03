af7551844232ec9c1cdf199b2822bd0dd7ca4f45
German Ordoñez <gaordonezh@gmail.com>
2020-11-03 16:46:41 -0500

    mlab fué agregado

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongodb: {
-        URI: 'mongodb://localhost:27017/node-login-passport-local'
+        URI: 'mongodb+srv://user_login:user_login_pass@cluster0.3g6kt.mongodb.net/Cluster0'
     }
 }
